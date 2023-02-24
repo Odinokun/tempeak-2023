8bb60cf9e2876ba712a074c739287e0c33e301cc
odinokun <odinokun@gmail.com>
2023-02-24 20:59:18 +0100

    commit

--- src/js/modules/banner.js
+++ src/js/modules/banner.js
@@ -1,5 +1,10 @@
 module.exports = function () {
 
+  $('.ad-banner__close').on('click', function (e) {
+    e.preventDefault();
+    $('.ad-banner').fadeOut();
+  });
+
   $('.banner__close').on('click', function (e) {
     e.preventDefault();
     $('.banner').fadeOut();
