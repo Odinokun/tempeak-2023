31d6f0f0c4230f4018346db44429f346d34730f2
odinokun <odinokun@gmail.com>
2023-03-07 23:00:49 +0100

    did all

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,5 +1,11 @@
 module.exports = function () {
+
   $('#burger input').on('click', function () {
-    $('#header').toggleClass('active')
+    $('#main-menu').toggleClass('active')
   });
+
+  $('.user-ava__img').on('click', function () {
+    $('#second-menu').toggleClass('active')
+  });
+
 };
